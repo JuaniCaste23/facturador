29a246e51b4a40564d5655cf22bd2e04113ab193
JuaniCaste23 <juanicaste23@gmail.com>
2021-11-23 22:21:40 -0300

    Implementacion de HTML

---
+++ script.js
@@ -0,0 +1,15 @@
+//Variables
+//Index del FORM
+const nombre = document.getElementById('nombre');
+const servicio = document.getElementById('servicio');
+const fecha = document.getElementById('fecha');
+const mes = document.getElementById('mes');
+const plan = document.getElementById('plan');
+const monto = document.getElementById('monto');
+const submit = document.getElementsByClassName('button_submit');
+const reset = document.getElementsByClassName('button_reset');
+
+//Funciones
+
+
+//Eventos
