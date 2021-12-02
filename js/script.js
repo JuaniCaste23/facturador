92fc3a955db0ee86d0064701ed3d4662661e5113
joshuacba08 <joshuacba08@gmail.com>
2021-12-01 23:10:20 -0300

    evento submit

--- js/script.js
+++ js/script.js
@@ -1,15 +1,35 @@
 //Variables
-//Index del FORM
-const nombre = document.getElementById('nombre');
-const servicio = document.getElementById('servicio');
-const fecha = document.getElementById('fecha');
-const mes = document.getElementById('mes');
-const plan = document.getElementById('plan');
-const monto = document.getElementById('monto');
-const submit = document.getElementsByClassName('button_submit');
-const reset = document.getElementsByClassName('button_reset');
+const form = document.getElementById("form-facturador");
+const order = {
+    servicio:"",
+    velocidad:"",
+    nombre:"",
+    fecha:"",
+    monto:0
+}
+
 
 //Funciones
 
+const setData = (e) => {
+    e.preventDefault();
+
+    for (let index = 0; index < 4; index++) {
+        if(e.target[`${index}`].checked ){
+            order.servicio=e.target[`${index}`].value;
+            continue;
+        }
+        //ayuda a setear la velodicidad
+        if(order.servicio==="internet" && index===1){
+            order.velocidad = e.target[`${index}`].value
+            continue;
+        }
+
+        console.log(e.target[`${index}`].value);
+    }
+}
+
 
 //Eventos
+
+form.addEventListener('submit',setData);
